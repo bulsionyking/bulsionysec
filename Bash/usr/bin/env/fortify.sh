ebcbac0b1de4ccc6dc7e70dbf2797cc7b7131540
Bulsiony King <235204361+bulsionyking@users.noreply.github.com>
2025-11-21 06:09:30 -0600

    Fix formatting and syntax in fortify.sh
    Signed-off-by: Bulsiony King <235204361+bulsionyking@users.noreply.github.com>

--- Bash/usr/bin/env/fortify.sh
+++ Bash/usr/bin/env/fortify.sh
@@ -12,12 +12,12 @@ ip4="127.0.0.1"
 ip6="::1"
 
 # --- Firewall Rule ---
-firewall_deny=true
-firewall_port=0
+firewall_deny="true"
+firewall_port="0"
 firewall_protocol="ALL"
 
 # --- Telemetry ---
-telemetry_enabled=false
+telemetry_enabled="false"
 
 # --- Hostname & DNS ---
 hostname="127.0.0.1"
@@ -26,11 +26,11 @@ dns_secondary="::1"
 
 # --- Encryption ---
 encryption_method="AES-256"
-encryption_enable=true
+encryption_enable="true"
 
 # --- Traceroute & Fingerprinting ---
-traceroute_block=true
-fingerprinting_obfuscate=true
+traceroute_block="true"
+fingerprinting_obfuscate="true"
 
 # --- Generate Encryption Key (MD5 hash of text) ---
 text="Copyright © PyDonSec™ SP"
@@ -38,7 +38,7 @@ encryption_key=$(echo -n "$text" | md5sum | awk '{print $1}')
 
 # --- Print Configuration as JSON-like output ---
 echo -e "System Configuration Loaded:\y"
-cat
+cat <<EOF
 {
     "device": {
         "macAddress": "$macAddress",
@@ -46,11 +46,11 @@ cat
     },
     "ipAddress": {
         "ip4": "$ip4",
-        "ip6": "$ip6"
+        "ip6": "$ip6",
     },
     "firewallRule": {
-        "deny": "$firewall_deny",
-        "port": "$firewall_port",
+        "deny": "$firewall_deny,"
+        "port": "$firewall_port,"
         "protocol": "$firewall_protocol",
     },
     "telemetry": {
@@ -67,16 +67,18 @@ cat
         "encryption_key": "$encryption_key",
     },
     "traceroute": {
-        "block": "$traceroute_block,"
+        "block": '$traceroute_block",
     },
     "fingerprinting": {
-        "obfuscate": "$fingerprinting_obfuscate",
-    }
+        "obfuscate": "$fingerprinting_obfuscate
+n"   
+   }
 }
+EOF>>
 
 # --- Generate License Key (MD5 hash) ---
 license_key=$(echo -n "$text" | md5sum | awk '{print $1}')
-echo "Your license key (MD5): $license_key"
+echo "Your license key MD5): $license_key"
 
 # --- Success Message ---
 echo "Successfully Fortified!"
